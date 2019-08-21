ddac7cf7b00c2e836f2f6fa1df5dcc1f57c0d8cd
fabianmarinog-turner <45363310+fabianmarinog-turner@users.noreply.github.com>
2019-08-21 16:04:10 -0500

    Includes custom event polyfill

--- get-own-property-symbols.js
+++ get-own-property-symbols.js
@@ -1,3 +1,30 @@
+(function () {
+        'use strict';
+
+        console.log('****custom-event****');
+
+        if (typeof window.CustomEvent === 'function' && typeof window.Event === 'function') {
+            return false;
+        }
+
+        function CustomEvent(event, params) {
+            var evt;
+
+            params = params || { bubbles: false, cancelable: false, detail: undefined };
+            evt = document.createEvent('CustomEvent');
+            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+            return evt;
+        }
+
+        CustomEvent.prototype = window.Event.prototype;
+
+        if (typeof window.CustomEvent !== 'function') {
+            window.CustomEvent = CustomEvent;
+        }
+        if (typeof window.Event !== 'function') {
+            window.Event = CustomEvent;
+        }
+    })();
 (function (Object, GOPS) {'use strict';
 
   // (C) Andrea Giammarchi - Mit Style
